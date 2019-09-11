25b0389da6728b0bac6234fc64b218099bbe75ac
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-09-11 15:17:53 +1000

    Done

--- Assets/Scripts/MazeConstructor.cs
+++ Assets/Scripts/MazeConstructor.cs
@@ -11,6 +11,30 @@ public class MazeConstructor : MonoBehaviour
     private MazeDataGenerator dataGenerator;
     private MazeMeshGenerator meshGenerator;
 
+    public float hallWidth
+    {
+        get; private set;
+    }
+    public float hallHeight
+    {
+        get; private set;
+    }
+    public int startRow
+    {
+        get; private set;
+    }
+    public int startCol
+    {
+        get; private set;
+    }
+    public int goalRow
+    {
+        get; private set;
+    }
+    public int goalCol
+    {
+        get; private set;
+    }
 
     public int[,] data
     {
@@ -31,17 +55,29 @@ public class MazeConstructor : MonoBehaviour
         meshGenerator = new MazeMeshGenerator();
     }
 
-    public void GenerateNewMaze(int sizeRows, int sizeCols)
+    public void GenerateNewMaze(int sizeRows, int sizeCols,
+    TriggerEventHandler startCallback = null, TriggerEventHandler goalCallback = null)
     {
         if (sizeRows % 2 == 0 && sizeCols % 2 == 0)
         {
             Debug.LogError("Odd numbers work better for dungeon size.");
         }
 
+        DisposeOldMaze();
+
         data = dataGenerator.FromDimensions(sizeRows, sizeCols);
+
+        FindStartPosition();
+        FindGoalPosition();
+
+        // store values used to generate this mesh
+        hallWidth = meshGenerator.width;
+        hallHeight = meshGenerator.height;
 
         DisplayMaze();
 
+        PlaceStartTrigger(startCallback);
+        PlaceGoalTrigger(goalCallback);
     }
 
     private void OnGUI()
@@ -91,5 +127,83 @@ public class MazeConstructor : MonoBehaviour
 
         MeshRenderer mr = go.AddComponent<MeshRenderer>();
         mr.materials = new Material[2] { mazeMat1, mazeMat2 };
+    }
+
+    public void DisposeOldMaze()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Generated");
+        foreach(GameObject go in objects)
+        {
+            Destroy(go);
+        }
+    }
+
+    void FindStartPosition()
+    {
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        for (int i = 0; i <= rMax; i++)
+        {
+            for (int j = 0; j <= cMax; j++)
+            {
+                if(maze[i,j] == 0)
+                {
+                    startRow = i;
+                    startCol = j;
+                    return;
+                }
+            }
+        }
+    }
+
+    private void FindGoalPosition()
+    {
+        int[,] maze = data;
+        int rMax = maze.GetUpperBound(0);
+        int cMax = maze.GetUpperBound(1);
+
+        // loop top to bottom, right to left
+        for (int i = rMax; i >= 0; i--)
+        {
+            for (int j = cMax; j >= 0; j--)
+            {
+                if (maze[i, j] == 0)
+                {
+                    goalRow = i;
+                    goalCol = j;
+                    return;
+                }
+            }
+        }
+    }
+
+    private void PlaceStartTrigger(TriggerEventHandler callback)
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = new Vector3(startCol * hallWidth, .5f, startRow * hallWidth);
+        go.name = "Start Trigger";
+        go.tag = "Generated";
+
+        go.GetComponent<BoxCollider>().isTrigger = true;
+        go.GetComponent<MeshRenderer>().sharedMaterial = startMat;
+
+        TriggerEventRouter tc = go.AddComponent<TriggerEventRouter>();
+        tc.callback = callback;
+    }
+
+    private void PlaceGoalTrigger(TriggerEventHandler callback)
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.position = new Vector3(goalCol * hallWidth, .5f, goalRow * hallWidth);
+        go.name = "Treasure";
+        go.tag = "Generated";
+
+        go.GetComponent<BoxCollider>().isTrigger = true;
+        go.GetComponent<MeshRenderer>().sharedMaterial = treasureMat;
+
+        TriggerEventRouter tc = go.AddComponent<TriggerEventRouter>();
+        tc.callback = callback;
     }
 }
